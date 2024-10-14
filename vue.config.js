4c318a0a451d60d073034cacdc069ecbf600b4a1
DESKTOP-NFFV85D\xt <xtlicc@outlook.com>
2024-10-14 13:25:54 +0800

    add

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,11 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/print_sku/' : '/',
+  outputDir: 'dist/print_sku',
+  assetsDir: '',
+  indexPath: 'index.html',
+  filenameHashing: true,
+  productionSourceMap: false,
+}
